904bf385527bd1d6b5693f3185c2a1cebc91ae47
Julian <home@Julians-MBP.lan>
2021-09-07 16:35:50 -0500

    '2021-Sep-07'

--- {{cookiecutter.project_slug}}/app-push.sh
+++ {{cookiecutter.project_slug}}/app-push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ../
-rsync -avz --exclude-from={{ cookiecutter.project_slug }}/exclude.txt {{ cookiecutter.project_slug }}/ root@{{cookiecutter.server_ip}}:/root/{{ cookiecutter.project_slug }}/
-sshpass -p "<SERVER PASSWORD HERE>" ssh -o StrictHostKeyChecking=no root@{{cookiecutter.server_ip}} "cd {{ cookiecutter.project_slug }}; docker-compose -f production.yml down; docker-compose -f production.yml up --build"
+SERVER_PASS='<SERVER PASSWORD HERE>'
+sshpass -p "$SERVER_PASS" rsync -avz --exclude-from={{ cookiecutter.project_slug }}/exclude.txt {{ cookiecutter.project_slug }}/ root@{{cookiecutter.server_ip}}:/root/{{ cookiecutter.project_slug }}/
+sshpass -p "$SERVER_PASS" ssh -o StrictHostKeyChecking=no root@{{cookiecutter.server_ip}} "cd {{ cookiecutter.project_slug }}; docker-compose -f production.yml down; docker-compose -f production.yml up --build"
